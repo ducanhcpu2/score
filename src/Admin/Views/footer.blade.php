79b7b9a32d9c597a8306533ae53e54f7e852add0
anhnd <anhnd@gmail.com>
2020-11-04 22:49:27 +0700

    first commit

---
+++ src/Admin/Views/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 
+    {{ config('s-cart.version') }}
+  </div>
+  <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
+  reserved.
+</footer>
